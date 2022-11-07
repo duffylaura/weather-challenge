a2e619dc820d6833a6216986eba8ce85dfdce18c
Coffee Pawz <lauralynneduffy@gmail.com>
2022-11-07 11:52:05 -0500

    Lat/lon api working. Current day's temp etc working.

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,14 @@
-var input = document.querySelector('#input');
-var searchBtn = document.querySelector('#searchBtn');
-var storage = document.querySelector('#storage');
-var cityName = document.querySelector ('#cityName');
-var todayForecast = document.querySelector('#todayForecast');
-var day1 = document.querySelector('#day1');
-var forecast1 = document.querySelector('#forecast1');
-var day2 = document.querySelector('#day2');
-var forecast2 = document.querySelector('#forecast2');
-var day3 = document.querySelector('#day3');
-var forecast3 = document.querySelector('#forecast3');
-var day4 = document.querySelector('#day4');
-var forecast4 = document.querySelector('#forecast4');
-var day5 = document.querySelector('#day5');
-var forecast5 = document.querySelector('#forecast5');
+var searchBtnEl = document.querySelector('.btn');
+
+searchBtnEl.addEventListener ("click", function(event){
+    event.preventDefault(); 
+    var input = $('#input').val();  
+    accessAPI(input);
+});
 
 //access geocode API based on user input 
-
 function accessAPI (input) {
-    var apiURL = `http://api.openweathermap.org/geo/1.0/direct?q={${input}}&limit={limit}&appid={31c7e1b3bd169805a458edfc303e0b06}`
+    var apiURL = 'http://api.openweathermap.org/geo/1.0/direct?q='+input+'&appid=31c7e1b3bd169805a458edfc303e0b06';
     fetch(apiURL)
     .then (function (response){
         if (response.ok) {
@@ -38,32 +29,58 @@ function coordinates (data) {
     var latitude = data[0].lat; 
     var longitude = data[0].lon; 
 
-    weather (latitude, longitude); 
+    weatherNow (latitude, longitude); 
+    //will need to call weatherlater here 
 };
 
-// Get the weather using forecast api 
-function weather (latitude, longitude) {
-    var apiURL = `api.openweathermap.org/data/2.5/forecast?lat={${latitude}}&lon={${longitude}}&appid={31c7e1b3bd169805a458edfc303e0b06}`
+//Get the weather using "call current weather data"
+
+function weatherNow (latitude, longitude) {
+   var apiURL = 'https://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+'&appid=31c7e1b3bd169805a458edfc303e0b06';
+   fetch (apiURL)
+   .then (function(response){
+    if(response.ok){
+        response.json().then(function(data){
+            //console.log(data);
+            var city = data.name; 
+            var temp = Math.round(((data.main.temp)-273.15)*(9/5)+32); //convert temp from K to F, round to nearest integer
+            var humidity = data.main.humidity; // measured as a %
+            var wind = data.wind.speed; // measured in mph
+            var iconTag = data.weather[0].icon; 
+            var iconLink = "https://openweathermap.org/img/wn/" + iconTag + "@2x.png";
+
+            generateWeatherNow (city, temp, humidity, wind, iconLink);
+        })}
+    else {alert("Error")}
+    })
+    .catch(function(err){ alert("Error")})
+};
+
+var cityName = document.querySelector('#cityName');
+var todayForecast = document.querySelector('#todayForecast');
+
+function generateWeatherNow(city, temp, humidity, wind, iconLink){
+    var heading = document.createElement('h1');
+    heading.textContent = city; 
+    cityName.appendChild(heading);
+};
+
+
+// Get the weather using forecast api for five day forecast
+function weatherLater (latitude, longitude) {
+    var apiURL = 'http://api.openweathermap.org/data/2.5/forecast?lat='+latitude+'&lon='+longitude+'&appid=31c7e1b3bd169805a458edfc303e0b06';
     fetch (apiURL)
     .then (function(response){
         if(response.ok) {
             response.json().then(function (data){
-                var temp = data[0].list.main.temp; 
-                var wind = data[0].list.main.wind.speed; 
-                var humidity = data[0].list.main.humidity; 
-                var icon = data[0].list.weather.icon; 
+                //var temp = data[0].list.main.temp; 
+                //var wind = data[0].list.main.wind.speed; 
+                //var humidity = data[0].list.main.humidity; 
+                //var icon = data[0].list.weather[0].icon; 
                 
-                console.log (temp); 
-                console.log(wind);
-                console.log(humidity); 
-                console.log(icon);
+                console.log(data);
 
             });
         };
     });
 };
-
-function init () {
-accessAPI("Paris");};
-
-init(); 
